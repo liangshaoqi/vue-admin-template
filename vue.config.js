f5ee573c542cb837e172a590bfe4e190dcee8c10
LiangShaoQi <liangshaoqi@shixinyun.com>
2019-03-20 10:16:38 +0800

    架构

---
+++ vue.config.js
@@ -0,0 +1,38 @@
+/**
+ * webpack配置文件
+ */
+var path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+module.exports = {
+  lintOnSave: true, // eslint开关
+  runtimeCompiler: true, // 是否使用包含运行时编译器的Vue内核版本
+  chainWebpack: config => {
+      // config.entry.app = [], // 入口文件
+      config.resolve.alias // 别名配置
+        .set("src", resolve("src"))
+        .set("api", resolve("src/api"))
+        .set("assets", resolve("src/assets"))
+        .set("components", resolve("src/components"))
+        .set("mixins", resolve("src/mixins"))
+        .set("router", resolve("src/router"))
+        .set("utils", resolve("src/utils"))
+        .set("views", resolve("src/views"))
+        .set("@vuex", resolve("src/vuex"));
+  },
+  css: {
+    // // 启用 CSS modules
+    // modules: false,
+    // // 是否使用css分离插件
+    // extract: true,
+    // // 开启 CSS source maps?
+    // sourceMap: false,
+    // // css预设器配置项
+    loaderOptions: {
+      sass: { // 导入公共样式
+        data: `@import "@/assets/styles/common.scss";`
+      }
+    }
+  }
+};
